8892ba8b59e07d7e1b10db6b71df94f9fce5f97d
John Duimovich <jduimovich@gmail.com>
2021-11-27 07:38:49 -0500

    fix cache cmd

--- cache-image.cmd
+++ cache-image.cmd
@@ -1,4 +1,4 @@
 docker login -u developer -p developer 
-pull docker.io/node 
+docker pull docker.io/node 
 docker tag docker.io/node default-route-openshift-image-registry.apps-crc.testing:5000/node
 docker push default-route-openshift-image-registry.apps-crc.testing:5000/node
